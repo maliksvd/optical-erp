bc58d2de9c0eaa48bf178c130c463c10d9f9106e
Malik <62953363+maliksvd@users.noreply.github.com>
2024-05-07 22:32:07 -0400

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Feather } from '@expo/vector-icons';
 import { Link, Tabs } from 'expo-router';
+import { BlurView } from 'expo-blur';
 import { Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
@@ -9,51 +10,60 @@ import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof Feather>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <Feather size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <BlurView intensity={100} style={{ flex: 1 }}>
+      <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        headerShown: false,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Orders',
+          tabBarIcon: ({ color }) => <TabBarIcon name="inbox" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="products"
+        options={{
+          title: 'Products',
+          tabBarIcon: ({ color }) => <TabBarIcon name="box" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="agenda"
+        options={{
+          title: 'Agenda',
+          tabBarIcon: ({ color }) => <TabBarIcon name="calendar" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="patients"
+        options={{
+          title: 'Patients',
+          tabBarIcon: ({ color }) => <TabBarIcon name="users" color={color} />,
         }}
       />
     </Tabs>
+    </BlurView>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,57 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView alwaysBounceVertical={false} showsVerticalScrollIndicator={false}>
+          <View style={styles.cardItems}>
+            <View style={styles.card}>
+              <Text style={styles.cardTitle}>16</Text>
+              <Text style={styles.cardText}>Orders</Text>
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.cardTitle}>16</Text>
+              <Text style={styles.cardText}>Manufacturing</Text>
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.cardTitle}>16</Text>
+              <Text style={styles.cardText}>Shipping</Text>
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.cardTitle}>16</Text>
+              <Text style={styles.cardText}>Delivered</Text>
+            </View>
+
+          </View>
+        </ScrollView>
+
+        <View style={styles.ordersCard}>
+          <Text>Orders</Text>
+          <View>
+            <Text>Order 1</Text>
+            <Text>Order 2</Text>
+            <Text>Order 3</Text>
+            <Text>Order 4</Text>
+            <Text>Order 5</Text>
+            <Text>Order 6</Text>
+            <Text>Order 7</Text>
+            <Text>Order 8</Text>
+            <Text>Order 9</Text>
+            <Text>Order 10</Text>
+            <Text>Order 11</Text>
+            <Text>Order 12</Text>
+            <Text>Order 13</Text>
+            <Text>Order 14</Text>
+            <Text>Order 15</Text>
+            <Text>Order 16</Text>
+          </View>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -17,15 +59,61 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 12,
+    justifyContent: 'flex-start',
+    paddingTop: StatusBar.currentHeight,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  card: {
+    backgroundColor: '#fff',
+    paddingVertical: 36,
+    paddingHorizontal: 18,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    borderRadius: 12,
+    width: 200,
+    height: 120,
+    marginRight: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05
+  },
+  ordersCard: {
+    backgroundColor: '#fff',
+    paddingVertical: 36,
+    paddingHorizontal: 18,
+    marginHorizontal: 12,
+    marginVertical: 12,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    borderRadius: 12,
+    width: '100%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  cardText: {
+    fontSize: 12,
+  },
+  cardItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 12,
+    marginHorizontal: 12,
   },
 });
